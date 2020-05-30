e8d6ddab24b545bc4277bf4dd84dd7e65228360c
Driss El mouedden <d.elmouedden@gmail.com>
2020-05-30 16:58:06 +0100

    Add Views Sale Order

--- webmania_projects/nutrisla/models/nutrisla.py
+++ webmania_projects/nutrisla/models/nutrisla.py
@@ -8,3 +8,9 @@ class SaleOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
     number_of_packs = fields.Integer('Nombre de packs')
+
+
+    @api.onchange('number_of_packs')
+    def set_product_uom(self):
+                pack = self.product_packaging
+                self.product_uom_qty = pack.qty * self.number_of_packs
